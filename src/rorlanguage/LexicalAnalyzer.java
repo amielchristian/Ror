718eeef7dca05c40b62e2ca2149ea0d83e5a2699
James Ivan Opao <jamesivan.opao.cics@ust.edu.ph>
2024-02-15 23:28:08 +0800

    Updated dfa.json

--- src/rorlanguage/LexicalAnalyzer.java
+++ src/rorlanguage/LexicalAnalyzer.java
@@ -4,7 +4,7 @@ import modules.DFALex;
 
 public class LexicalAnalyzer {
     public static void main(String[] args)  {
-        String s = "\"Hello\"";
+        String s = "1_aod_mwoak";
         String filename = "dfa.json";
         
         DFALex dfa = new DFALex(filename);
