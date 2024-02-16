e3a4afef970aad39e6f01140e83a99e0e149a80d
James Ivan Opao <jamesivan.opao.cics@ust.edu.ph>
2024-02-16 09:21:17 +0800

    Updated Test Cases, Fixed Tester

--- src/rorlanguage/LexicalAnalyzer.java
+++ src/rorlanguage/LexicalAnalyzer.java
@@ -22,34 +22,31 @@ public class LexicalAnalyzer {
     
     static void runTestCases(DFALex dfa) {
         BufferedReader br;
-            String str;
+            String input, expected;
             String[] strArray;
             boolean matched;
             try {
                 System.out.println("VALID RESERVED WORDS / TOKENS");
                 br = new BufferedReader(new FileReader( "TestCases/keywords.txt"));
-                while ((str = br.readLine()) != null) {
-                    strArray = str.split(",");
-                    matched = dfa.run(strArray[0].trim());
-                    System.out.println("Input Token: " + strArray[0].trim() + ", Expected: " + strArray[1].trim() + "\nSTATUS: " + (matched ? "PASSED" : "FAILED") + "\n");
+                while ((input = br.readLine()) != null && (expected = br.readLine()) != null) {
+                    matched = dfa.run(input.trim());
+                    System.out.println("Input Token: " + input.trim() + ", Expected: " + expected.trim() + "\nSTATUS: " + (matched ? "PASSED" : "FAILED") + "\n");
                 }
                 
                 System.out.println("\n\n\n");
                 System.out.println("VALID LITERALS / IDENTIFIERS");
                 br = new BufferedReader(new FileReader( "TestCases/validTokens.txt"));
-                while ((str = br.readLine()) != null) {
-                    strArray = str.split(",");
-                    matched = dfa.run(strArray[0].trim());
-                    System.out.println("Input Token: " + strArray[0].trim() + ", Expected: " + strArray[1].trim() + "\nSTATUS: " + (matched ? "PASSED" : "FAILED") + "\n");
+                while ((input = br.readLine()) != null && (expected = br.readLine()) != null) {
+                    matched = dfa.run(input.trim());
+                    System.out.println("Input Token: " + input.trim() + ", Expected: " + expected.trim() + "\nSTATUS: " + (matched ? "PASSED" : "FAILED") + "\n");
                 }
                 
                 System.out.println("\n\n\n");
                 System.out.println("INVALID TOKENS");
                 br = new BufferedReader(new FileReader( "TestCases/invalidTokens.txt"));
-                while ((str = br.readLine()) != null) {
-                    strArray = str.split(",");
-                    matched = dfa.run(strArray[0].trim());
-                    System.out.println("Input Token: " + strArray[0].trim() + "\nSTATUS: " + (matched ? "FAILED" : "PASSED") + "\n");
+                while ((input = br.readLine()) != null) {
+                    matched = dfa.run(input.trim());
+                    System.out.println("Input Token: " + input.trim() + "\nSTATUS: " + (matched ? "FAILED" : "PASSED") + "\n");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
