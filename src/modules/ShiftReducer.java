8dd63fdca4454ad686df82606ccab6df7630ecf2
James Ivan Opao <jamesivan.opao.cics@ust.edu.ph>
2024-03-01 10:00:10 +0800

    Added Shift Reducer

---
+++ src/modules/ShiftReducer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modules;
+
+/**
+ *
+ * @author Ivan
+ */
+public class ShiftReducer {
+    public ShiftReducer() {}
+    public void reduce(DFALex dfa, String statement) {
+        
+    }
+}
--- src/rorlanguage/LexicalAnalyzer.java
+++ src/rorlanguage/LexicalAnalyzer.java
@@ -14,62 +14,11 @@ public class LexicalAnalyzer {
         String dfaFile = "dfa.json";
         
         DFALex dfa = new DFALex(dfaFile);
-//        runTestCases(dfa);
-//        Scanner sc = new Scanner(System.in);
-//        String input;
-//        while(true) {
-//            System.out.print("INPUT: ");
-//            input = sc.nextLine();
-//            dfa.run(input);
-//        }
-        runTestProgram(dfa);
+//        runTestProgram(dfa);
+    
     }
     
-//    static void runTestCases(DFALex dfa) {
-//        BufferedReader br;
-//            String input, expected;
-//            String[] strArray;
-//            String output = "output";
-//            boolean matched;
-//            try {
-//                output += "VALID RESERVED WORDS / TOKENS";
-//                System.out.println("VALID RESERVED WORDS / TOKENS");
-//                br = new BufferedReader(new FileReader( "TestCases/keywords.txt"));
-//                while ((input = br.readLine()) != null && (expected = br.readLine()) != null) {
-//                    matched = dfa.run(input.trim());
-//                    System.out.println("Input Token: " + input.trim() + ", Expected: " + expected.trim() + "\nSTATUS: " + (matched ? "PASSED" : "FAILED") + "\n");
-//                    output += "\nInput Token: " + input.trim() + ", Expected: " + expected.trim() + "\nSTATUS: " + (matched ? "PASSED" : "FAILED") + "\n";
-//                }
-//                
-//                
-//                output += "\n\n\nVALID LITERALS / IDENTIFIERS";
-//                System.out.println("\n\n\n");
-//                System.out.println("VALID LITERALS / IDENTIFIERS");
-//                br = new BufferedReader(new FileReader( "TestCases/validTokens.txt"));
-//                while ((input = br.readLine()) != null && (expected = br.readLine()) != null) {
-//                    matched = dfa.run(input.trim());
-//                    System.out.println("Input Token: " + input.trim() + ", Expected: " + expected.trim() + "\nSTATUS: " + (matched ? "PASSED" : "FAILED") + "\n");
-//                    output += "\nInput Token: " + input.trim() + ", Expected: " + expected.trim() + "\nSTATUS: " + (matched ? "PASSED" : "FAILED") + "\n";
-//                }
-//                
-//                System.out.println("\n\n\n");
-//                System.out.println("INVALID TOKENS");
-//                output += "\n\n\n INVALID TOKENS";
-//                br = new BufferedReader(new FileReader( "TestCases/invalidTokens.txt"));
-//                while ((input = br.readLine()) != null) {
-//                    matched = dfa.run(input.trim());
-//                    System.out.println("Input Token: " + input.trim() + "\nSTATUS: " + (matched ? "FAILED" : "PASSED") + "\n");
-//                    output += "\nInput Token: " + input.trim() + "\nSTATUS: " + (matched ? "FAILED" : "PASSED") + "\n";
-//                }
-//                
-//                PrintWriter writer = new PrintWriter("output.txt");
-//                writer.println(output);
-//                writer.close();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//    }
-//    
+
     static void runTestProgram(DFALex dfa) {;
         String str, token;
         String output = "";
@@ -105,6 +54,10 @@ public class LexicalAnalyzer {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
+    }
+    
+    static void runShiftReduce(DFALex dfa) {
         
     }
     
